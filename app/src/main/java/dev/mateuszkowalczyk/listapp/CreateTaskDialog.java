3752416001dc0e3ba3a0c73a4ca5f9a2a202eb96
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2019-08-31 13:16:44 +0200

    Add bottom sheet to add task

---
+++ app/src/main/java/dev/mateuszkowalczyk/listapp/CreateTaskDialog.java
@@ -0,0 +1,20 @@
+package dev.mateuszkowalczyk.listapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreateTaskDialog extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/dev/mateuszkowalczyk/listapp/activity/MainActivity.java
+++ app/src/main/java/dev/mateuszkowalczyk/listapp/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package dev.mateuszkowalczyk.listapp.activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import dev.mateuszkowalczyk.listapp.CreateTaskDialog;
 import dev.mateuszkowalczyk.listapp.R;
 import dev.mateuszkowalczyk.listapp.fragments.TaskFragment;
 import dev.mateuszkowalczyk.listapp.models.Task;
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity implements TaskFragment.OnLi
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floating_button_add_new_task);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateTaskDialog bottomSheetDialog = new CreateTaskDialog();
+                bottomSheetDialog.show(getSupportFragmentManager(), "createTaskBottomSheet");
+            }
+        });
 
     }
 
